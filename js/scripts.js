1f914e8f15da2a69f729f62222aa15582b07e2a2
Carlos Munoz Kampff and Dan Lauby <carlos@>
2017-01-09 10:53:43 -0800

    create calculator files, repositories, HTML template and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function add (number1, number2) {
+	return number1 + number2;
+}
+
+var number1 = parseFloat(prompt("first number to add"));
+
+var number2 = parseFloat(prompt("second number to add"));
+
+alert(add(number1, number2));
