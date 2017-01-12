a16e6a773db6d323950f7ad30f58fb6892b842f6
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-01-11 16:58:00 -0800

    add UI with forms for adding and major chord calculation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-var number1 = parseFloat(prompt("enter first number"));
-var number2 = parseFloat(prompt("enter second number"));
 
+/////////////// business logic ////////////////////////
 function add (number1, number2) {
 	return number1 + number2;
 }
@@ -17,8 +16,34 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
-// alert(add(number1, number2));
+function majorChord(startingNote) {
+	var startingNote = parseInt($("#startingNote")val());
+  var noteNumber = parseInt(startingNote) % 12;
+  var thirdNumber = noteNumber + 4;
+  var fifthNumber = noteNumber + 7;
+  return "Root: " + noteNumber + "\nThird: " + thirdNumber + "\nFifth: " + fifthNumber;
+}
 
+////////////////user interface ///////////////
+
+$(document).ready(function() {
+
+
+	$(".numberInput").submit(function(event){
+		event.preventDefault ();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		// debugger;
+		// alert(add(number1, number2));
+		$("#result-label").text(result)
+
+	});
+
+});
+
+// alert(add(number1, number2));
+//alert(majorChord(number1));
 // alert(subtract(number1, number2));
 // alert(multiply(number1, number2));
 // alert(divide(number1, number2));
